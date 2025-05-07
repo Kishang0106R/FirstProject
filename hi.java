79b7239fa10dd20594c80823ac8802f6e2e1b106
FirstProject <iamkishansingh27@gmail.com>
2025-05-07 11:56:34 +0530

    new changes in hi.java

---
+++ hi.java
@@ -0,0 +1,12 @@
+package FirstProject;
+
+public class hi {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is my first Java program.");
+        System.out.println("I am learning Java programming.");
+        System.out.println("This is a simple Java program.");
+        System.out.println("I hope to learn more about Java.");
+    }
+    
+}
